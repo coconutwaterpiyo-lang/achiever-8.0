ec48168ed0969a473dbd86ab541f0b1d32fa585c
coconutwaterpiyo-lang <coconutwaterpiyo@gmail.com>
2026-06-17 23:10:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+async function loadFolders() {
+    const response = await fetch("data.json");
+    const data = await response.json();
 
+    const container = document.getElementById("folders");
+    container.innerHTML = "";
+
+    data.folders.forEach(folder => {
+        const div = document.createElement("div");
+        div.className = "folder";
+        div.innerHTML = `
+            <div class="folder-name">📁 ${folder.name}</div>
+            <div class="arrow">➜</div>
+        `;
+        container.appendChild(div);
+    });
+}
+
+loadFolders();
